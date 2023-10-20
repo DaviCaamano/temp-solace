1d3fbd11a972414a6e1b0ef48861fbab9803a01c
Davi Caamano <davi.caamano@shiftpixy.com>
2023-10-20 19:57:04 -0400

    debugging prod

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,6 +2,8 @@ import { Header } from '@components/header/Header';
 import { Content } from '@components/landing/Content';
 
 export default function Web() {
+
+    console.log('AUTH0_BASE_URL', process.env.AUTH0_BASE_URL)
   return (
     <div id={'home-page'} className={'flex flex-col-reverse flex-1 min-w-screen relative mb-8'}>
       <Content />
