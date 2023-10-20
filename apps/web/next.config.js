0f27556d5e25b450637c0a73cb46c092c065fcc0
Davi Caamano <davi.caamano@shiftpixy.com>
2023-10-20 15:17:55 -0400

    stable turbo-repo with next app router

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,4 @@
-const withTM = require("next-transpile-modules")(["ui"]);
-
-module.exports = withTM({
-  reactStrictMode: true,
-});
+module.exports = {
+    reactStrictMode: true,
+    transpilePackages: ["ui"]
+};
--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   content: [
     "../../packages/ui/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/screens/**/*.{js,ts,jsx,tsx}",
+    "./src/providers/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
