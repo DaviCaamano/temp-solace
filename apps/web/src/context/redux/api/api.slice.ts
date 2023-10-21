a88eeff9a9bc3052909f8834431a62f5ad401637
Davi Caamano <davi.caamano@shiftpixy.com>
2023-10-20 21:41:18 -0400

    debugging prod

--- apps/web/src/context/redux/api/api.slice.ts
+++ apps/web/src/context/redux/api/api.slice.ts
@@ -18,7 +18,7 @@ type ApiSlice = Api<
   typeof coreModuleName | typeof reactHooksModuleName
 >;
 
-const baseUrl = process.env.BASE_URL;
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 console.log(
   '~~~~~~~~~~~',
   baseUrl + '/api/',
