d6da3c880aacdf570dad1352fc86f356464c2fbf
Davi Caamano <davi.caamano@shiftpixy.com>
2023-10-20 20:53:37 -0400

    debugging prod

--- apps/web/src/app/api/auth/[auth0]/route.js
+++ apps/web/src/app/api/auth/[auth0]/route.js
@@ -1,3 +1,8 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+console.log(process.env.AUTH0_BASE_URL);
+export const GET = handleAuth({
+  onError(req, error) {
+    console.error(error);
+  },
+});
